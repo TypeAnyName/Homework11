d7207c6b56b9cef343385de5b0bc8d17276cf9d4
TypeAnyName <alexgox11@gmail.com>
2022-03-21 15:56:55 +0300

    Настроил поиск папки templates, создан файл code.html

---
+++ part1/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,template_folder="../templates")
+
+
+@app.route('/', )
+def page_index():
+    name = "Ivan"
+    number = "132134"
+    return render_template("code.html", name=name, number=number)
+
+
+app.run()
